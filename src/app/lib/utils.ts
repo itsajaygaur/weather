8ea9f80c99cc68bf6e4bdafe2588917741729a57
ajay <ajaypathak2527@gmail.com>
2024-05-15 15:39:55 +0530

    fix error in utils:

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -1,13 +1,13 @@
 import { weatherIconByCode } from '@/constants/data';
 
 export function getWeatherIcon(weatherCode: string){
-    const icon = JSON.parse(`${weatherIconByCode}`)[weatherCode]?.day.image
+    const icon = JSON.parse(JSON.stringify(weatherIconByCode))[weatherCode]?.day.image 
     if(icon) return icon
     return "https://placehold.co/600x400?text=:("
 }
 
 export function getWeatherDescription(weatherCode: string){
-    const description = JSON.parse(`${weatherIconByCode}`)[weatherCode]?.day.description
+    const description = JSON.parse(JSON.stringify(weatherIconByCode))[weatherCode].day.description
     if(description) return description
     return "No description"
 }
